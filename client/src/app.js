07019a79265172920bc2c90c4335543a1404a131
José Enrique Lázaro Galván <jenrilaga@gmail.com>
2022-09-28 23:42:27 -0500

    first commit

---
+++ client/src/app.js
@@ -0,0 +1,165 @@
+import './App.css';
+import io from 'socket.io-client'
+import { useEffect, useState } from 'react';
+
+//'http://localhost:4000'
+
+const socket = io('http://localhost:4000')
+
+function App() {
+
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+    const [date, setDate] = useState(new Date())
+
+
+    const [user, setUser] = useState('')
+    const [isLogged, setIsLogged] = useState(false)
+    const [products, setProducts] = useState([])
+    const [product, setProduct] = useState({
+        title: '',
+        price: ''
+    })
+
+
+
+
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([message, ...messages])
+        })
+
+        socket.on('loadMessages', (dataMessages) => setMessages(dataMessages))
+
+        socket.on('loadProducts', (dataProducts) => setProducts(dataProducts))
+
+
+        setDate(new Date().toLocaleTimeString())
+
+    }, [messages])
+
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const mensaje = {
+            body: message,
+            from: user
+        }
+
+        if (message) {
+            socket.emit('message', mensaje)
+            const newMessage = {
+                body: message,
+                from: 'Me',
+
+            }
+            setMessages([newMessage, ...messages])
+            setMessage('')
+
+        }
+
+    }
+
+    const handleLogged = () => (user) && setIsLogged(true)
+
+    const handleProduct = (e) => {
+
+        e.preventDefault()
+
+        console.log(product)
+        socket.emit('products', product)
+
+        setProducts([...products, product])
+
+
+
+    }
+
+    useEffect(() => {
+        socket.on('product', (data) => setProducts([...product, data]))
+    }, [product])
+
+
+    console.log(products)
+    console.log(messages)
+
+    return (
+        <div className="App">
+
+            <form className='form-product' onSubmit={(e) => handleProduct(e)}>
+                <h2>Productos</h2>
+                <div className='container-controlls products'>
+                    <input type="text" name='title' onChange={(e) => setProduct({ ...product, [e.target.name]: e.target.value })} placeholder='Ingresa un producto' required />
+                    <input type="text" name='price' onChange={(e) => setProduct({ ...product, [e.target.name]: e.target.value })} placeholder='Ingresa un precio' required />
+
+                    <button type="submit" >Ingresar</button>
+                </div>
+                {products.length > 0 && (
+                    <table>
+                        <tr>
+                            <th>Title</th>
+                            <th>Price</th>
+                        </tr>
+                        {products.map(product => (
+                            <tr key={product.title} className='product'>
+
+                                <th>{product.title}</th>
+                                <th>{product.price}</th>
+                            </tr>
+                        ))}
+
+                    </table>
+                )}
+
+
+            </form>
+
+
+
+            <form className='form-chat' onSubmit={(e) => handleSubmit(e)} >
+
+
+
+                {!isLogged ? (
+                    <div className='container-login'>
+                        <input type="text" onChange={(e) => setUser(e.target.value)} placeholder='Ingresa tu nombre para chatear' required />
+                        <button type="submit" onClick={() => handleLogged()}>Ingresar</button>
+                    </div>
+
+                ) : (
+                    <div className='chat-room'>
+                        <div className="container-controlls">
+                            <input type="text" value={message} onChange={e => setMessage(e.target.value)} placeholder='Ingrese un mensaje' />
+                            <button type="submit">Enviar</button>
+                        </div>
+
+                        <ul className="messages-container">
+                            {messages.map((message, index) => (
+                                <div className={`message ${message.from}`} key={index}>
+                                    <li >{message.from}: {message.body}</li>
+                                    <span>{date}</span>
+                                </div>
+                            ))}
+
+                        </ul>
+
+                    </div>
+
+                )}
+
+
+
+
+
+            </form>
+
+
+        </div>
+    );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,101 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
+import { PORT } from './config.js';
+import cors from 'cors';
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url';
+import fs, { readFile } from 'fs'
+
+const dataMessages = [];
+const dataProducts = [];
+//READ MESSAGES
+
+const readFiles = async (file) => {
+    try {
+        const fileData = await fs.promises.readFile(file, "utf-8")
+
+        const array = JSON.parse(fileData)
+        array.forEach(message => fileData === "./messages.txt" ? dataMessages.push(message) : dataProducts.push(message))
+
+
+
+    } catch (error) {
+        console.log(error)
+
+    }
+
+}
+
+console.log(dataMessages)
+
+//SAVE MESSAGES
+const save = async (data, file) => {
+    try {
+        const writteMessages = await fs.promises.writeFile(file, JSON.stringify(data))
+
+        return console.log(`Mensaje enviado exitosamente `);
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+};
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname)
+const httpServer = http.createServer(app)
+const io = new Server(httpServer, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+})
+app.use(cors())
+
+io.on('connection', (socket) => {
+
+    readFiles("./messages.txt")
+    readFiles("./products.txt")
+
+    socket.emit('loadMessages', dataMessages)
+    socket.emit('loadProducts', dataProducts)
+
+
+    socket.on('message', (mensaje) => {
+
+        dataMessages.push(mensaje)
+        console.log(dataMessages)
+        save(dataMessages, "./messages.txt");
+
+        socket.broadcast.emit('message', {
+            body: mensaje.body,
+            from: mensaje.from
+        })
+    })
+
+    socket.on('products', (products) => {
+        dataProducts.push(products)
+        console.log(dataProducts)
+        save(dataProducts, "./products.txt");
+
+        socket.broadcast.emit('product', {
+            title: products.title,
+            price: products.price
+        })
+    })
+
+    socket.on('disconnect', (socket) => {
+        console.log('cliente desconectado')
+    })
+
+
+
+
+})
+
+app.use(express.static(join(__dirname, '../socket/build')))
+
+httpServer.listen(PORT, () => {
+    console.log(`escuchando puerto ${PORT}`)
+})
